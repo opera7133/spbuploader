6e92aed6f78b4043f468e7ef46bb025c716fb726
opera7133 <ainznino@pm.me>
2020-07-23 19:36:52 +0900

    ogp

--- upload.php
+++ upload.php
@@ -5,6 +5,12 @@
     <title>追加 | SPBUploader</title>
     <link rel="icon" type="image/png" href="/favicon.png">
     <meta charset="utf-8">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@scgame_m" />
+    <meta property="og:url" content="https://spb.uploader.work" />
+    <meta property="og:title" content="SPBUploader" />
+    <meta property="og:description" content="Simple Sparebeat Chart Uploader" />
+    <meta property="og:image" content="/spbuploader.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
 </head>
